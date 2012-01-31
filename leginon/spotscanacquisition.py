a59fc5bcb5a54320e933a74b50ca6c531a0cdae0
spartango@gmail.com <spartango@gmail.com>
2012-01-31 14:23:47 -0500

    Added a box for spotsize in a new SpotAcquisiton Panel

--- leginon/spotscanacquisition.py
+++ leginon/spotscanacquisition.py
@@ -14,13 +14,21 @@ class SpotScanAcquisition(acquisition.Acquisition):
     def __init__(self, id, session, managerlocation, **kwargs):
         acquisition.Acquisition.__init__(self, id, session, managerlocation, **kwargs)
         # Spot series info
+        # TODO
 
     def setImageFilename(self, imagedata):
         setImageFilename(imagedata)
         # Spot series filenames
+        # TODO
 
     def processTargetList(self, targetlist):
         # Check the target list
+        targets = self.researchTargets(list=targetlist, status='new')
+        if not targets:
+            self.reportTargetListDone(tilt0targetlist, 'success')
+            return
+
+        # Get the spot size parameters
 
         # Generate a sub target list
 
