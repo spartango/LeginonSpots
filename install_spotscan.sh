3463b77e3819260ee88bf95fcda93a7d6f4f49ca
Imager <brushedglass@localhost.localdomain>
2012-02-07 12:36:47 -0500

    fixed install script

--- install_spotscan.sh
+++ install_spotscan.sh
@@ -6,5 +6,6 @@ echo Installing Spot Scan
 cp leginon/spotscanacquisition.py /usr/lib/python2.6/site-packages/leginon/
 cp leginon/gui/wx/SpotScanAcquisition.py /usr/lib/python2.6/site-packages/leginon/gui/wx/
 cp leginon/allnodes.py /usr/lib/python2.6/site-packages/leginon/
+cp leginon/leginondata.py /usr/lib/python2.6/site-packages/leginon/
 
 echo Done Installing
