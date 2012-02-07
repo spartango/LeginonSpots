1e3e173deef72228a71b7cb19a46bddfea68f91a
spartango@gmail.com <spartango@gmail.com>
2012-02-07 13:34:44 -0500

    minor change to try and set TEM spotsize by hand?

--- leginon/spotscanacquisition.py
+++ leginon/spotscanacquisition.py
@@ -151,16 +151,12 @@ class SpotScanAcquisition(acquisition.Acquisition):
                         except acquisition.InvalidStagePosition:
                             return 'invalid'
 
-                        presetdata = self.presetsclient.getPresetByName(newpresetname)
-                        old_spotsize = presetdata['spot size']
-                        presetdata['spot size'] = spotsize
+                        self.instrument.tem.SpotSize = spotsize
 
                         ### acquire film or CCD
                         self.startTimer('acquire')
                         ret = self.acquire(presetdata, emtarget, attempt=attempt, target=subtarget)
                         self.stopTimer('acquire')
-
-                        presetdata['spot size'] = old_spotsize
 
                         # in these cases, return immediately
                         if ret in ('aborted', 'repeat'):
@@ -172,73 +168,3 @@ class SpotScanAcquisition(acquisition.Acquisition):
         self.reportStatus('processing', 'Processing complete')
 
         return ret
-
-    # Overrides
-    def _processTargetList(self, targetlist):
-        # Check the target list
-        targets = self.researchTargets(list=targetlist, status='new')
-        if not targets:
-            self.reportTargetListDone(targetlist, 'success')
-            return
-
-        # Get the spot size parameters
-        spotsize    = self.settings['spot size']
-        spotcount   = self.settings['spot count']
-        spotspacing = self.settings['spot spacing']
-
-        # Check that these are sane
-        if spotsize >= 9 or spotsize <= 1 or spotcount < 0:
-            self.logger.error('Invalid spot parameters')
-            return
-
-        # Generate a sub target list
-        newtargets = []
-        # For each target
-
-        self.logger.info('Generating sub-targets');
-
-        for target in targets:
-
-        #   Bounding box from camera frame
-            bound_x, bound_y = targetShape(target)
-
-        #   Grab coordinates for the target
-            center_x, center_y = targetPoint(target)
-
-        #   Corner points
-            start_x = center_x - (spotcount/2) * spotspacing
-            start_y = center_y - (spotcount/2) * spotspacing
-
-            end_x = center_x + (spotcount/2) * spotspacing
-            end_y = center_y + (spotcount/2) * spotspacing
-
-            self.logger.info('Subtargets for '+center_x+', '+center_y)
-            self.logger.info('Interval: '+start_x+', '+start_y+' -> '+end_x+', '+end_y)
-        
-        #   Generate new coordinates around point
-            for point_x in range(start_x, end_x, spotspacing):# left bound to right bound
-                for point_y in range(start_y, end_y, spotspacing): # top to bottom bound
-                    
-                    # Check that coordinates are in frame
-                    if point_x > 0.0 and point_x < bound_x and point_y > 0.0 and point_y < bound_y:
-                        sub_target = leginondata.AcquisitionImageTargetData(initializer=target)
-                        sub_target['delta row']    = point_x
-                        sub_target['delta column'] = point_y
-
-                        new_targets.append(sub_target)
-
-        spottargetlist = self.newTargetList(image=targets.image)
-
-        self.publish(spottargetlist, database=True)
-
-        for spottarget in newtargets:
-            spottarget['list']  = spottargetlist
-            spottarget['image'] = targets.image
-            spottarget['scope'] = targets.image['scope']
-            self.publish(spottarget, database=True)
-
-        # set the spot size
-        self.instrument.tem['spot size'] = spotsize
-
-        # Call processTargetList
-        acquisition.Acquisition.processTargetList(spottargetlist)
